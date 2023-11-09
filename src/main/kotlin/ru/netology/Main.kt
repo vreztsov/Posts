41e61ac7c43bf62e48aec0bdac8d06a79d0e965a
vreztsov <vreztsov90@mail.ru>
2023-11-09 23:26:47 +0300

    Add other properties of Post

--- src/main/kotlin/ru/netology/Main.kt
+++ src/main/kotlin/ru/netology/Main.kt
@@ -36,22 +36,73 @@ data class ViewsInfo(
     val count: Int = 0
 )
 
+data class Copyright(
+    val id: Int,
+    val link: String,
+    val name: String,
+    val type: String
+)
+
+data class PostSource(
+    val type: String,
+    val platform: String,
+    val data: String,
+    val url: String
+)
+
+data class Geo(
+    val type: String,
+    val coordinates: String,
+    val place: Place
+)
+
+data class Place(
+    val id: Int,
+    val title: String,
+    val latitude: Int,
+    val longitude: Int,
+    val created: Int,
+    val icon: String,
+    val country: String,
+    val city: String
+)
+
+data class Donut(
+    val isDonut: Boolean,
+    val paidDuration: Int,
+    val placeholder: Any,
+    val canPublishFreeCopy: Boolean,
+    val editMode: String
+)
+
 data class Post(
-    val id: Int = 0,
+    val id: Int? = null,
     val ownerId: Int,
     val fromId: Int,
     val createdBy: Int,
     val date: Int,
     val text: String,
+    val replyOwnerId: Int? = null,
+    val replyPostId: Int? = null,
     val friendsOnly: Boolean = false,
     val comments: CommentsInfo = CommentsInfo(),
+    val copyright: Copyright? = null,
     val likes: LikesInfo = LikesInfo(),
     val reposts: RepostsInfo = RepostsInfo(),
     val views: ViewsInfo = ViewsInfo(),
     val postType: String = "post",
+    val postSource: PostSource? = null,
+    val geo: Geo? = null,
+    val signerId: Int? = null,
+    val copyHistory: Array<Post> = emptyArray(),
     val canPin: Boolean = false,
     val canDelete: Boolean = true,
-    val canEdit: Boolean = true
+    val canEdit: Boolean = true,
+    val isPinned: Boolean = false,
+    val markedAsAds: Boolean = false,
+    val isFavourite: Boolean = false,
+    val donut: Donut? = null,
+    val postponedId: Int? = null
 )
 
 object WallService {
